857cca602e4a05bae82f552ff20b99e74317ed0f
Wang Yucai <netowls-studio@outlook.com>
2023-01-16 16:32:14 +0800

    feat: 新增资源配置文件。

--- docs/package-dependencies-description.md
+++ docs/package-dependencies-description.md
@@ -3,6 +3,7 @@
 - **Development Dependency**
   - [vite@^4.0.4](https://www.npmjs.com/package/vite)
   - [vue@^3.2.45](https://www.npmjs.com/package/vue)
-  - [vite-plugin-html^3.2.0](https://www.npmjs.com/package/vite-plugin-html)
+  - [vite-plugin-html@^3.2.0](https://www.npmjs.com/package/vite-plugin-html)
+  - [typescript@^4.9.3](https://www.npmjs.com/package/typescript)
 
 ![NPM](../assets/Icons/Npm.svg)
---
+++ src/sdk/resources/src/resources/index.ts
@@ -0,0 +1,9 @@
+// LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+// COPYRIGHT © 2006 - 2023 WANG YUCAI.
+
+import EnglishResourceManifest from "./en-us.json";
+import SimplifiedChineseResourceManifest from "./zh-cn.json";
+import TraditionalChineseResourceManifest from "./zh-tw.json";
+
+export { SimplifiedChineseResourceManifest, EnglishResourceManifest, TraditionalChineseResourceManifest };
+
