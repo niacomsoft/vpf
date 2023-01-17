e3ba347fbd3659b539a7b9842dd8fab3492a3820
Wang Yucai <netowls-studio@outlook.com>
2023-01-17 11:19:44 +0800

    feat: 新增 String 类型静态扩展方法定义。

--- docs/package-dependencies-description.md
+++ docs/package-dependencies-description.md
@@ -5,7 +5,9 @@
   - [vue@^3.2.45](https://www.npmjs.com/package/vue)
   - [vite-plugin-html@^3.2.0](https://www.npmjs.com/package/vite-plugin-html)
   - [typescript@^4.9.3](https://www.npmjs.com/package/typescript)
+  - [@types/string-template@^1.0.2](https://www.npmjs.com/package/@types/string-template)
 - **Dependency**
   - [fast-deep-equal@^3.1.3](https://www.npmjs.com/package/fast-deep-equal)
+  - [string-template@^1.0.0](https://www.npmjs.com/package/string-template)
 
 ![NPM](../assets/Icons/Npm.svg)
--- src/sdk/common/typings/lib.global.d.ts
+++ src/sdk/common/typings/lib.global.d.ts
@@ -33,7 +33,7 @@ declare global {
 		 *
 		 * @param {*} obj 需要校验的对象实例或值。
 		 * @returns {boolean}
-		 * @see {@link __isUndefined__}
+		 * @see {@link Object.isUndefined}
 		 */
 		isNull(obj: any): boolean;
 
@@ -44,9 +44,60 @@ declare global {
 		 * @param {*} obj1 需要校验的对象实例或值。
 		 * @param {*} obj2 需要校验的对象实例或值。
 		 * @returns {boolean}
-		 * @see {@link __isUndefined__}
-		 * @see {@link __isNull__}
+		 * @see {@link Object.isNull}
+		 * @see https://www.npmjs.com/package/fast-deep-equal
 		 */
 		referenceEquals(obj1: any, obj2: any): boolean;
+	}
+
+	/**
+	 * 为 String 类型提供的静态扩展方法。
+	 * @author Wang Yucai
+	 *
+	 * @interface StringConstructor
+	 * @typedef {StringConstructor}
+	 */
+	interface StringConstructor {
+		/**
+		 * 获取一个字符串，用于表示空字符串。
+		 * @author Wang Yucai
+		 *
+		 * @readonly
+		 * @type {string}
+		 */
+		empty: string;
+
+		/**
+		 * @description 验证字符串 {@link s} 是否为 undefined 类型值、null 值或 {@link String.empty}。
+		 * @author Wang Yucai
+		 *
+		 * @param {string} s 需要验证的字符串。
+		 * @returns {boolean}
+		 * @see {@link Object.isNull}
+		 */
+		isNullOrEmpty(s: string): boolean;
+
+		/**
+		 * @description 验证字符串 {@link s} 是否为 undefined 类型值、null 值、{@link String.empty}或者空格符。
+		 * @author Wang Yucai
+		 *
+		 * @param {string} s 需要验证的字符串。
+		 * @returns {boolean}
+		 * @see {@link String.isNullOrEmpty}
+		 */
+		isNullOrWhiteSpace(s: string): boolean;
+
+		/**
+		 * 格式化字符串。使用方法参考 .NET System.String.Format 方法。
+		 * @author Wang Yucai
+		 *
+		 * @param {string} formatter 格式化字符串模板。
+		 * @param {...Array<any>?} [args] 格式化参数数组。
+		 * @returns {string}
+		 * @see https://learn.microsoft.com/zh-cn/dotnet/api/system.string.format?view=net-7.0#system-string-format(system-string-system-object())
+		 * @see https://www.npmjs.com/package/@types/string-template
+		 * @see https://www.npmjs.com/package/string-template
+		 */
+		format(formatter: string, ...args: Array<any>): string;
 	}
 }
