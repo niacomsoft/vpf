07cea42ce529d2eb5ba06506183c5ed1c20dd643
Wang Yucai <netowls-studio@outlook.com>
2023-01-18 09:09:05 +0800

    refactor: 删除了不合理的代码。

--- src/sdk/common/typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-// LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
-// COPYRIGHT © 2006 - 2023 WANG YUCAI.
-
-/// <reference path="./lib.global.d.ts" />
-/// <reference path="./lib.types.d.ts" />
-/// <reference path="./lib.functions.d.ts" />
-/// <reference path="./lib.interfaces.d.ts" />
--- src/sdk/resources/src/resources/index.ts
+++
@@ -1,9 +0,0 @@
-// LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
-// COPYRIGHT © 2006 - 2023 WANG YUCAI.
-
-import EnglishResourceManifest from "./en-us.json";
-import SimplifiedChineseResourceManifest from "./zh-cn.json";
-import TraditionalChineseResourceManifest from "./zh-tw.json";
-
-export { SimplifiedChineseResourceManifest, EnglishResourceManifest, TraditionalChineseResourceManifest };
-
